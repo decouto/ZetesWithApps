282816db0f7c4c94ea9ef664e38efc54d8e1760f
Benjamin Chrobot <bchrobot@mit.edu>
2013-04-30 00:01:50 -0400

    MultimediaAdapter started

---
+++ appsolut/src/com/appsolut/composition/utils/MultimediaAdapter.java
@@ -0,0 +1,37 @@
+package com.appsolut.composition.utils;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class MultimediaAdapter extends BaseAdapter {
+    
+    public MultimediaAdapter() {
+        
+    }
+
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
