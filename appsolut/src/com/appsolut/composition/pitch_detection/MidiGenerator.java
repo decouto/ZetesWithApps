ac1944868d1713ebd0b5faaee8b0d2e741506284
David C Danko <dcdanko@gmail.com>
2013-04-30 15:14:09 -0400

    actually this time

--- appsolut/src/com/appsolut/composition/pitch_detection/MidiGenerator.java
+++ appsolut/src/com/appsolut/composition/pitch_detection/MidiGenerator.java
@@ -66,11 +66,11 @@ public class MidiGenerator {
 		Pair<Integer[],Long[]> analysedAudio = new Pair<Integer[],Long[]>();
 		
 		double readVal = audio_stream.readDouble();
-		while(readVal > .00001){
+		while(readVal+1 > .00001){
 			if(audio[0] < .00001){
 				start = 0;
 			}
-			for(int i=slide;i<audio.length && readVal > .00001;i++){
+			for(int i=slide;i<audio.length && readVal+1 > .00001;i++){
 				audio[i] = readVal;
 				readVal = audio_stream.readDouble();
 			}
